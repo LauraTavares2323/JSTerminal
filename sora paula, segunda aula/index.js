a7e8990263e0a1e75be3824d5f4923fa6c505f2f
Laura Tavares <144948480+LauraTavares2323@users.noreply.github.com>
2025-04-04 23:06:22 -0300

    Add files via upload

---
+++ sora paula, segunda aula/index.js
@@ -0,0 +1,41 @@
+//primeira atividade, criar variaveis 
+
+let nome = "Laura"
+nome ="Laura Tavares"
+console.log(nome)
+
+
+let sobrenome = "Tavares"
+sobrenome = "Tavares"
+console.log(sobrenome)
+
+let idade = 12
+idade = "12 anos"
+console.log(idade)
+
+let emprego = "Empregado"
+emprego = "Desempregado"
+console.log(emprego)
+
+
+// exemplos de como imprimir no inspecionar 
+
+console.log(nome + sobrenome + idade + emprego)
+
+console.log("Meu nome é", nome, "E meu sobrenome é", sobrenome, "minha idade é", idade, "anos. Estou empregado:", emprego)
+
+console.log(`Meu nome é ${nome} e meu sobrenome é ${sobrenome} minha idade é ${idade} anos. Estou empregado ${emprego}`)
+
+
+// Segundo exercicio 
+// typeof diz o tipo da variavel
+const Nomee = prompt("Digite o seu nome")
+
+let Idadee = Number(prompt("Digite a sua idade"))
+
+console.log(typeof Nomee)
+console.log(typeof Idadee)
+
+idade.toString()
+console.log(typeof Idadee)
+console.log(typeof Nomee)
